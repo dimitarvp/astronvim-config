0ca8b6ea7d1d855828ba2fe7cbfbe973858dcaac
Dimitar Panayotov <mitko.p@gmail.com>
2024-05-18 16:27:47 +0300

    remove superfluous nesting

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,32 +1,30 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      integrations = {
-        sandwich = false,
-        noice = true,
-        mini = true,
-        leap = true,
-        markdown = true,
-        neotest = true,
-        cmp = true,
-        overseer = true,
-        lsp_trouble = true,
-        rainbow_delimiters = true,
-      },
-      flavour = "mocha",
-      dim_inactive = { enabled = true, percentage = 0.25 },
-      highlight_overrides = {
-        mocha = function(c)
-          return {
-            Normal = { bg = c.mantle },
-            Comment = { fg = "#7687a0" },
-            ["@tag.attribute"] = { style = {} },
-          }
-        end,
-      },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    integrations = {
+      sandwich = false,
+      noice = true,
+      mini = true,
+      leap = true,
+      markdown = true,
+      neotest = true,
+      cmp = true,
+      overseer = true,
+      lsp_trouble = true,
+      rainbow_delimiters = true,
+    },
+    flavour = "mocha",
+    dim_inactive = { enabled = true, percentage = 0.25 },
+    highlight_overrides = {
+      mocha = function(c)
+        return {
+          Normal = { bg = c.mantle },
+          Comment = { fg = "#7687a0" },
+          ["@tag.attribute"] = { style = {} },
+        }
+      end,
     },
   },
 }
