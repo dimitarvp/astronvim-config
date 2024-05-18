4383af45f4734be64177b4ba919a7aa1224b0d34
Dimitar Panayotov <mitko.p@gmail.com>
2024-05-18 17:21:26 +0300

    customize Telescope: prompt prefix, scroll strategy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,37 @@ local actions = require "telescope.actions"
 return {
   "nvim-telescope/telescope.nvim",
   opts = {
+    defaults = {
+      -- layout_config = {
+      --   -- prompt_position = "top",
+      --   height = 0.90,
+      --   width = 0.90,
+      --   bottom_pane = {
+      --     height = 25,
+      --     preview_cutoff = 120,
+      --   },
+      --   center = {
+      --     height = 0.4,
+      --     preview_cutoff = 40,
+      --     width = 0.5,
+      --   },
+      --   cursor = {
+      --     preview_cutoff = 40,
+      --   },
+      --   horizontal = {
+      --     preview_cutoff = 120,
+      --     preview_width = 0.6,
+      --   },
+      --   vertical = {
+      --     preview_cutoff = 40,
+      --   },
+      --   flex = {
+      --     flip_columns = 150,
+      --   },
+      -- },
+      prompt_prefix = " ",
+      scroll_strategy = "limit",
+    },
     pickers = {
       buffers = {
         initial_mode = "insert",
