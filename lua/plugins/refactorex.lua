3f033bbdbc72abd74ac6e8c302a205998111dc7d
Dimitar Panayotov <mitko.p@gmail.com>
2025-01-24 04:28:07 +0200

    add `refactorex` manager

---
+++ lua/plugins/refactorex.lua
@@ -0,0 +1,10 @@
+return {
+  "synic/refactorex.nvim",
+  ft = "elixir",
+  ---@module "refactorex.nvim"
+  ---@type refactorex.Config
+  opts = {
+    auto_update = true,
+    pin_version = nil,
+  },
+}
