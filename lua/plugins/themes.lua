530994df5da2683c17d1f3c70c5afc44e9cb31ab
Dimitar Panayotov <mitko.p@gmail.com>
2025-08-05 23:28:00 +0300

    add themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,18 +1,38 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    flavour = "mocha",
-    dim_inactive = { enabled = true, percentage = 0.25 },
-    highlight_overrides = {
-      mocha = function(c)
-        return {
-          Normal = { bg = c.mantle },
-          Comment = { fg = "#7687a0" },
-          ["@tag.attribute"] = { style = {} },
-        }
-      end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      dim_inactive = { enabled = true, percentage = 0.25 },
+      no_italic = true,
+      highlight_overrides = {
+        mocha = function(c)
+          return {
+            Normal = { bg = c.mantle },
+            Comment = { fg = "#7687a0" },
+            ["@tag.attribute"] = { style = {} },
+          }
+        end,
+      },
+    },
+  },
+  {
+    "2giosangmitom/nightfall.nvim",
+    lazy = true,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = true,
+  },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "deep",
+      code_style = {
+        comments = "none",
+      },
     },
   },
 }
